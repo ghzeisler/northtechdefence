e12859246ad999b66ad38ad6eb862090aef30ee3
Dustin Zeisler <d@zyvo.us>
2013-05-18 17:04:00 -0700

    build

--- source/javascripts/bootstrap/bootstrap.js
+++
@@ -1,7 +0,0 @@
-//= require bootstrap/bootstrap-carousel.js
-//= require bootstrap/bootstrap-button
-//= require bootstrap/bootstrap-collapse
-//= require bootstrap/bootstrap-typeahead
-//= require bootstrap/bootstrap-modal
-
-
